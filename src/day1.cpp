36e0d85fd9f9d5918e1e8e50d28fe1e35b01b0b6
Victor <victor@poseidonos.com>
2022-12-02 14:06:01 -0800

    day 1 done

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,57 +1,43 @@
 #include "../inc/day1.h"
 
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <iterator>
-#include <fstream>
-#include <vector>
-#include <algorithm> 
 #include <stdint.h>
+#include <vector>
+#include <algorithm>
 
 std::vector<uint64_t> totalCalories;
 
 void ReadInNumbers()
 {
     std::ifstream is("../input/day1Input.txt");
-    std::istream_iterator<uint64_t> start(is), end;
-    std::vector<uint64_t> calorieCounts(start, end);
-    std::cout << "Read " << calorieCounts.size() << " numbers" << std::endl;
+    std::string currLine;
+    int localSum = 0;
+    while (std::getline(is, currLine))
+    {
+        // Line contains string of length > 0 then save it in vector
+        if(currLine.size() > 0)
+        {
+            localSum += stoi(currLine);
+        }
+        else
+        {
+            totalCalories.push_back(localSum);
+            localSum = 0;
+        }
+    }
+
+    std::sort(totalCalories.begin(), totalCalories.end());
+    std::cout << "wait";
+
 }
 
 std::ifstream is("numbers.txt");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 void dayOne()
 {
     std::cout << "hello one";
+    ReadInNumbers();
 }
-
